61ddc6750eac077b65d56d1d1d4315bbac816e9b
mnmoawiye <masoudmnm21@gmail.com>
2026-03-05 12:44:35 +0000

    added inventory files

---
+++ inventory.py
@@ -0,0 +1,8 @@
+def add_item(inventory, item):
+    pass
+
+def remove_item(inventory, item):
+    pass
+
+def get_item_count(inventory):
+    pass
---
+++ test_inventory.py
@@ -0,0 +1,3 @@
+import pytest
+from inventory import add_item, remove_item, get_item_count
+
